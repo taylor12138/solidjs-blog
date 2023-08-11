d794fa9529b906f75786375b2d9b179f76fecbe7
linzizhan <linzizhan@corp.netease.com>
2023-08-11 15:44:38 +0800

    feat: build name

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,20 @@ import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [
+    base: './',
+    plugins: [
     /* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     // devtools(),
     solidPlugin(),
-  ],
-  server: {
+    ],
+    server: {
     port: 3000,
-  },
-  build: {
+    },
+    build: {
     target: 'esnext',
-    outDir: 'docs'
-  },
+    outDir: 'docs',
+    },
 });
