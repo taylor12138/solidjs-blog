bdda1ded12a2a911be3947cc46d1ba1532a44460
linzizhan <linzizhan@corp.netease.com>
2023-11-28 10:12:32 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import type { Component } from 'solid-js';
 
 import logo from './logo.svg';
-import styles from './App.module.css';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class='w-[100%] flex items-center justify-center'>
+        <div class='nav flex items-center h-[40px] w-[400px] justify-around'>
+            <div>主页</div>
+            <div>博客</div>
+            <div>请联系我</div>
+        </div>
     </div>
   );
 };
---
+++ src/pages/tab1.tsx
@@ -0,0 +1,6 @@
+import { render } from 'solid-js/web';
+
+export function HelloWorld() {
+  return <div>Hello World!</div>;
+}
+
